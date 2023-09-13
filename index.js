1e6de80e04dc56f14cfb6c3210e1ba7a78c9b0e5
mephaline <suaramos88@gmail.com>
2023-09-12 21:15:33 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const yourName = 'Sua Ramos'; 
+newHeader.textContent = `${yourName} is the champion`;
+
+
+const body = document.querySelector('body');
+body.appendChild(newHeader);
